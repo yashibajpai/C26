5f2c382593e6064244efa47de2efc921b9bc3f10
yashibajpai <yashi.bajpai@gmail.com>
2020-12-27 12:32:39 -0500

    platform added

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var engine, world;
 var box1, pig1;
 var backgroundImg;
 
+var platform;
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -17,6 +19,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(160, 305, 300, 170)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +62,7 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
+
